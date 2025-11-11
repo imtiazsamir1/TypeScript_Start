4faa3335871fb00d88b23e6a298e8e7eb0b01e3d
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-11 14:36:17 +0600

    assertion

---
+++ problem.ts
@@ -0,0 +1,17 @@
+//! function
+// function gun (a:number,b:number):number
+// {
+// return a*b
+// }
+// console.log(gun(3,5))
+
+//!interface
+
+interface User{
+    id:number;
+    name:string;
+    email?:string //?optional
+
+
+}
+type ID=string|number
