c44a340d54f771d21f254d79dc86fdf852a830e1
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-16 15:12:29 +0600

    abastrac

--- src/OOP/src/recap.ts
+++ src/OOP/src/recap.ts
@@ -17,6 +17,43 @@ person2.getId()
 
 
 //!inheritance
+// class Animal{
+//     constructor(public name:string,public spasis:string,public sound:string){
+        
+
+//     }
+//     makeSound(){
+//         console.log(`the animal name is${this.name}spsis is ${this.spasis} and sound is ${this.sound}`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(public name:string,public spasis:string,public sound:string, public age:number){
+//         super(name,spasis,sound)
+//         this.age=age;
+
+//     }
+//     getDog(){
+//         console.log('this is dog class')
+//     }
+// }
+
+// class Cat extends Animal{
+//     constructor(public name:string,public spasis:string,public sound:string, public color:string){
+//         super(name,spasis,sound)
+//         this.color=color;
+
+//     }
+//     getCat(){
+//         console.log(`this cat name is ${this.name } and her color is ${this.color}`)
+//     }
+// }
+
+// const cat1=new Cat('bilai','animal','meaw','white');
+// cat1.getCat()
+
+
+//! polimarphism
+
 class Animal{
     constructor(public name:string,public spasis:string,public sound:string){
         
@@ -32,8 +69,8 @@ class Dog extends Animal{
         this.age=age;
 
     }
-    getDog(){
-        console.log('this is dog class')
+     makeSound(){
+        console.log(`the name is ${this.name} & age is ${this.age}`)
     }
 }
 
@@ -43,10 +80,54 @@ class Cat extends Animal{
         this.color=color;
 
     }
-    getCat(){
+    makeSound(){
         console.log(`this cat name is ${this.name } and her color is ${this.color}`)
     }
 }
 
 const cat1=new Cat('bilai','animal','meaw','white');
-cat1.getCat()
+const dog1=new Dog('kutta','animal','burk burk',12);
+cat1.makeSound()
+dog1.makeSound()
+
+//!Abstruction
+
+// interface Multimedia{
+//   play():void;
+//   puse():void;
+//   stop():void;
+// }
+
+// class  Musicplayer implements Multimedia{
+//     play(): void {
+//         console.log('this is play......')
+//     }
+//     puse(): void {
+//         console.log("this is puse...")
+//     }
+//     stop(): void {
+//         console.log("this is stop")
+//     }
+// }
+// const musicPlayer1=new Musicplayer()
+// musicPlayer1.play()
+//? abastrac class
+
+abstract class MediaPlayer{
+    abstract play():void
+    abstract puse():void
+    abstract stop():void
+}
+class SamirPlayer extends MediaPlayer{
+    play(): void {
+        console.log("this is paly...")
+    }
+    puse(): void {
+        console.log("this is puse...")
+    }
+    stop(): void {
+        console.log("this is stop...")
+    }
+}
+const samirPlayer1 = new SamirPlayer()
+samirPlayer1.play()
