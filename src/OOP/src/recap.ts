929205f979476eef01a059edb3a210e7f55617e1
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-16 12:30:29 +0600

    inherit

--- src/OOP/src/recap.ts
+++ src/OOP/src/recap.ts
@@ -13,4 +13,40 @@ const person1= new Person("tanvir", 29,"amulia")
 person1.getId()
 const person2=new Person("Imran-vhai",40,"mirPara")
 person2.getId()
+
+
+
 //!inheritance
+class Animal{
+    constructor(public name:string,public spasis:string,public sound:string){
+        
+
+    }
+    makeSound(){
+        console.log(`the animal name is${this.name}spsis is ${this.spasis} and sound is ${this.sound}`)
+    }
+}
+class Dog extends Animal{
+    constructor(public name:string,public spasis:string,public sound:string, public age:number){
+        super(name,spasis,sound)
+        this.age=age;
+
+    }
+    getDog(){
+        console.log('this is dog class')
+    }
+}
+
+class Cat extends Animal{
+    constructor(public name:string,public spasis:string,public sound:string, public color:string){
+        super(name,spasis,sound)
+        this.color=color;
+
+    }
+    getCat(){
+        console.log(`this cat name is ${this.name } and her color is ${this.color}`)
+    }
+}
+
+const cat1=new Cat('bilai','animal','meaw','white');
+cat1.getCat()
